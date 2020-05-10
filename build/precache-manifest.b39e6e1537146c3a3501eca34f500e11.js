98f8cb0bee1a74a80d6b93ebf2e8ec62816e505a
github username <github email address>
2020-05-10 01:27:17 -0700

    working build

---
+++ build/precache-manifest.b39e6e1537146c3a3501eca34f500e11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c38493e86e43bef6ccd477e95c178682",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fd8cdd722af2db14a10",
+    "url": "/static/css/main.9d8eae8c.chunk.css"
+  },
+  {
+    "revision": "8502cc80ddefa03cc799",
+    "url": "/static/js/2.115ede10.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.115ede10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fd8cdd722af2db14a10",
+    "url": "/static/js/main.b20d4346.chunk.js"
+  },
+  {
+    "revision": "666b0ea7f714ad1d78ed",
+    "url": "/static/js/runtime-main.42b7a874.js"
+  }
+]);
